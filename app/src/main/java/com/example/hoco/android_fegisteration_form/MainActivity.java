750de6e96be6a87b2b6059eb8ebb26eaa493d54f
alimfaraji <hoco.hc@gmail.com>
2015-08-05 20:38:19 +0430

    UI + few codes in main activity

--- app/src/main/java/com/example/hoco/android_fegisteration_form/MainActivity.java
+++ app/src/main/java/com/example/hoco/android_fegisteration_form/MainActivity.java
@@ -1,48 +1,78 @@
 package com.example.hoco.android_fegisteration_form;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void registerUser(String username, String password) throws Exception{
+        SharedPreferences shP = getSharedPreferences("userPassPref", Context.MODE_PRIVATE);
+        if (shP.contains(username))
+            throw new Exception();
+        SharedPreferences.Editor editor = shP.edit();
+        editor.putString(username, password);
+        editor.commit();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Log.d(msg, "The onCreate() event");
+
+        EditText userNameField = (EditText)findViewById(R.id.username_field);
+        final String userNameString = userNameField.getText().toString();
+
+        EditText passwordField = (EditText)findViewById(R.id.password_field);
+        final String passwordString = passwordField.getText().toString();
+
+        Button signUpButton = (Button)findViewById(R.id.sign_up);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    registerUser(userNameString, passwordString);
+                    Toast.makeText(MainActivity.this, R.string.sign_up_message, Toast.LENGTH_LONG).show();
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this, R.string.sign_up_message_fail, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        Button signInButton = (Button)findViewById(R.id.sign_in);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isValidUserAndPass(userNameString, passwordString)){
+                    Toast.makeText(MainActivity.this, R.string.sign_in_message, Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(this, showNameActivity.class);
+//                    MainActivity.this.startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, R.string.sign_in_message_fail, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-//    protected  void onStart(){
-//        super.onStart();
-//        Log.d(msg, "The onStart() event");
-//    }
-//
-//    protected void onResume(){
-//        super.onResume();
-//        Log.d(msg, "The onResume() event");
-//    }
-//
-//    protected void onPause(){
-//        super.onPause();
-//        Log.d(msg, "The onPause() event");
-//    }
-//
-//    protected void onStop(){
-//        super.onStop();
-//        Log.d(msg, "The onStop() event");
-//    }
-//
-//    protected void onDestroy(){
-//        super.onDestroy();
-//        Log.d(msg, "The onDestroy() event");
-//    }
-//
-//    protected  void onRestart(){
-//        super.onRestart();
-//        Log.d(msg, "The onRestart() event");
-//    }
+    private boolean isValidUserAndPass(String username, String password){
+        SharedPreferences shP = getSharedPreferences("userPassPref", Context.MODE_PRIVATE);
+//        String ret = shP.getString(username, password);
+//        if (ret == null)
+//            return false;
+//        return true;
+        if (shP.contains(username))
+            return true;
+        return false;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
