ab9f9f4e9baf047f9e91ff6affe0abb6b4e0017c
alimfaraji <hoco.hc@gmail.com>
2015-08-05 13:27:10 +0430

    another update

--- app/src/main/java/com/example/hoco/android_fegisteration_form/MainActivity.java
+++ app/src/main/java/com/example/hoco/android_fegisteration_form/MainActivity.java
@@ -2,16 +2,47 @@ package com.example.hoco.android_fegisteration_form;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Log.d(msg, "The onCreate() event");
     }
+
+//    protected  void onStart(){
+//        super.onStart();
+//        Log.d(msg, "The onStart() event");
+//    }
+//
+//    protected void onResume(){
+//        super.onResume();
+//        Log.d(msg, "The onResume() event");
+//    }
+//
+//    protected void onPause(){
+//        super.onPause();
+//        Log.d(msg, "The onPause() event");
+//    }
+//
+//    protected void onStop(){
+//        super.onStop();
+//        Log.d(msg, "The onStop() event");
+//    }
+//
+//    protected void onDestroy(){
+//        super.onDestroy();
+//        Log.d(msg, "The onDestroy() event");
+//    }
+//
+//    protected  void onRestart(){
+//        super.onRestart();
+//        Log.d(msg, "The onRestart() event");
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
