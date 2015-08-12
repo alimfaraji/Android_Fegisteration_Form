cb249c67665b4c73ecec8988718b86d75b2ab679
DornaAbdolazimi <dorna.abdolazimi@gmail.com>
2015-08-13 03:48:34 +0430

    layouts

--- app/src/main/java/com/example/hoco/android_fegisteration_form/SplashScreenActivity.java
+++ app/src/main/java/com/example/hoco/android_fegisteration_form/SplashScreenActivity.java
@@ -13,7 +13,7 @@ import android.os.Handler;
 import java.util.logging.LogRecord;
 
 public class SplashScreenActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
